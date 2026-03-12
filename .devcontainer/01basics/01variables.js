ef3f33d409d2a290289bfff16b2e4a80bde5140a
Muhammad Anamur Rashid <muhammadanamurrashid@gmail.com>
2026-03-12 12:01:08 +0000

    container config

---
+++ .devcontainer/01basics/01variables.js
@@ -0,0 +1,11 @@
+const accountId = 8612
+let accountEmail = "muhammadanamurrashid@gmail.com"
+var accountPassword ="1234"
+accountCity =" ctg"
+//accountId = 8712
+accountEmail="anam@gmail.com"
+accountPassword="2345"
+accountCity="dhaka"
+let accountState
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
